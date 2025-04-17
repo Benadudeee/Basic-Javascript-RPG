c1cbe3109c713b0f08c7b382c77216b25a234fa9
Benedict A <sbenedict637@gmail.com>
2025-04-17 18:17:31 -0400

    Added more data for the description

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,76 +2,93 @@ import Skill from "../scripts/skill.js";
 
 const skills = [
     {
-        name: "Attack",
-        type: "damage",
+     name: "Attack",
+     type: "damage",
+     description: "A quick and forceful strike that deals light damage.",
+  
+     value: 10,
+     cost: 3,
+  
+     effects: [],
+     self_effect: null,
+  
+    },
+    {
+      name: "Smash",
+      type: "damage",
+      description: "A quick and forceful strike that deals light damage.",
 
-        value: 10,
-        cost: 0,
+      value: 15,
+      cost: 3,
 
-        effects: [
-            {status: "sample_effect", chance: 1, turns: 2}
-        ]
+      effects: [],
+      self_effect: null,
 
     },
     {
-        name: "Heal",
-        type: "heal",
+      name: "Heal",
+      type: "heal",
+      description: "Heal yourself",
 
-        value: 40,
-        cost: 4,
+      value: 25,
+      cost: 10,
 
-        effects: []
+      effects: [],
+      self_effect: null,
+
     },
     {
-        name: "Smash",
-        type: "damage",
+      name: "Barrage",
+      type: "damage",
+      description: "A flurry of rapid attacks that deals moderate damage.",
 
-        value: 15,
-        cost: 3,
-        
-        effects: []
+      value: 20,
+      cost: 7,
+
+      effects: [],
+      self_effect: null,
+
     },
     {
-        name: "Barrage",
-        type: "damage",
+      name: "Fireball",
+      type: "damage",
+      description: "Launch a blazing fireball that scorches enemies for heavy damage.",
 
-        value: 20,
-        cost: 7,
+      value: 35,
+      cost: 12,
 
-        effects: []
+      effects: [],
+      self_effect: null,
+
     },
     {
-        name: "Fireball",
-        type: "damage",
+      name: "Thunder",
+      type: "damage",
+      description: "Call down a bolt of lightning that deals damage and may stun the target.",
 
-        value: 35,
-        cost: 12,
+      value: 10,
+      cost: 8,
 
-        effects: []
+      effects: [
+        { status: "stunned", chance: 0.2, turns: 1 }
+      ],
+      self_effect: null,
+
     },
     {
-        name: "Thunder",
-        type: "damage",
+      name: "Annihilation",
+      type: "damage",
+      description: "Unleash devastating power that deals massive damage and may afflict the enemy with a deadly curse.",
 
-        value: 10,
-        cost: 8,
+      value: 70,
+      cost: 25,
 
-        effects: [
-            {status: "stunned", chance: 0.2, turns: 1}
-        ]
-    },
-    {
-        name: "Annihilation",
-        type: "damage",
-
-        value: 70,
-        cost: 25,
-
-        effects: [
-            {status: "calamity2", chance: 0.4, turns: 3},
-        ]
-    },
-]
+      effects: [
+        { status: "calamity2", chance: 0.4, turns: 3 }
+      ],
+      self_effect: null
+    }
+  ]
 
 /**
  * Gets the data of the skill by it's name
