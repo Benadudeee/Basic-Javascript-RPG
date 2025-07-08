5a8cb7d6afc5aed41ca7045568b8b189375e0437
Benedict A <sbenedict637@gmail.com>
2025-07-07 21:39:20 -0400

    Removed (commented out) selection screen

--- src/core/main.js
+++ src/core/main.js
@@ -1,131 +1,136 @@
 import Player from "../Player/player.js";
 import {Weapon, Armor} from "../Items/equipment.js";
 import Battle from "../Scenes/Battle/battle.js";
-import Dungeon from "../Scenes/dungeon.js";
+import Dungeon from "../Scenes/Dungeons/dungeon.js";
 
 import {characters, getCharacterByRole } from "../Player/player-data.js";
-import {enemies, getEnemyByName} from "../Enemies/enemy-data.js";
 import {armors, getArmor, getWeapon} from "../Items/equipments.js";
+
+import Goblin from "../Enemies/Goblin/goblin.js";
 
 const game = document.querySelector(".game");
 
+const goblin = new Goblin();
+goblin.health -= goblin.health * 0.75; // 25% HP Left
 
+console.log(goblin.skills);
+console.log(goblin.foo());
 
-const difficulties = [
-    {
-        category: "Cakewalk",
-        enemyDamageMod: 1.5,
-        playerDamageMod: .5,
-    },
-    {
-        category: "Easy",
-        enemyDamageMod: 1.25,
-        playerDamageMod: .75,
-    },
-    {
-        category: "Medium",
-        enemyDamageMod: 1,
-        playerDamageMod: 1
-    },
-    {
-        category: "Hard",
-        enemyDamageMod: .75,
-        playerDamageMod: 1.25,
-    },
-    {
-        category: "Supreme",
-        enemyDamageMod: .75,
-        playerDamageMod: 2
-    }
+// const difficulties = [
+//     {
+//         category: "Cakewalk",
+//         enemyDamageMod: 1.5,
+//         playerDamageMod: .5,
+//     },
+//     {
+//         category: "Easy",
+//         enemyDamageMod: 1.25,
+//         playerDamageMod: .75,
+//     },
+//     {
+//         category: "Medium",
+//         enemyDamageMod: 1,
+//         playerDamageMod: 1
+//     },
+//     {
+//         category: "Hard",
+//         enemyDamageMod: .75,
+//         playerDamageMod: 1.25,
+//     },
+//     {
+//         category: "Supreme",
+//         enemyDamageMod: .75,
+//         playerDamageMod: 2
+//     }
 
-]
+// ]
 
-const {category, enemyDamageMod, playerDamageMod} = difficulties[0];
+// const {category, enemyDamageMod, playerDamageMod} = difficulties[0];
 
-console.log(category, enemyDamageMod, playerDamageMod);
+// console.log(category, enemyDamageMod, playerDamageMod);
 
-class Main{
-    constructor(){
-        this.area = 'Plains';
-        this.difficulty = "medium";
-        this.player;
-        this.enemy;
-    }
+// class Main{
+//     constructor(){
+//         this.area = 'Plains';
+//         this.difficulty = "medium";
+//         this.player;
+//         this.enemy;
+//     }
 
-    selectCharacter(){
-        game.innerHTML =   
-        `
-        <div class="char-container">
-        <h1>Select Your Character</h1>
+//     selectCharacter(){
+//         game.innerHTML =   
+//         `
+//         <div class="char-container">
+//         <h1>Select Your Character</h1>
         
-        <div class="player-options">${characters.map(p => `<p>${p.name}</p>`).join("")} </div>
-        </div>
-        `
+//         <div class="player-options">${characters.map(p => `<p>${p.name}</p>`).join("")} </div>
+//         </div>
+//         `
         
-        document.querySelectorAll(".player-options > p").forEach((player, i) => {
-            player.addEventListener("click", () => {
-                // Equip the obliterator (uncomment for...... Idk)
-                const starterSet = new Armor(getArmor("starter_set"));
-                const starterWeapon = new Weapon(getWeapon("iron_pole"))
+//         document.querySelectorAll(".player-options > p").forEach((player, i) => {
+//             player.addEventListener("click", () => {
+//                 // Equip the obliterator (uncomment for...... Idk)
+//                 const starterSet = new Armor(getArmor("starter_set"));
+//                 const starterWeapon = new Weapon(getWeapon("iron_pole"))
 
-                this.player = new Player(characters[i]); 
-                this.player.equipArmor(starterSet);
-                this.selectDifficulty();
-            });
-        })
-    }
+//                 this.player = new Player(characters[i]); 
+//                 this.player.equipArmor(starterSet);
+//                 this.selectDifficulty();
+//             });
+//         })
+//     }
 
-    selectDifficulty(){
+    // selectDifficulty(){
         
-        game.innerHTML =  `
-            <div class="difficulty-container">
-                <h1> Select Difficulty </h1>
-                <div class="difficulty-options"> ${difficulties.map(element => `<p>${element.category}</p>`).join('  ')}</div>
-            </div>
-        `
+    //     game.innerHTML =  `
+    //         <div class="difficulty-container">
+    //             <h1> Select Difficulty </h1>
+    //             <div class="difficulty-options"> ${difficulties.map(element => `<p>${element.category}</p>`).join('  ')}</div>
+    //         </div>
+    //     `
 
-        const difficultiesOptions = document.querySelectorAll(".difficulty-options > p");
+    //     const difficultiesOptions = document.querySelectorAll(".difficulty-options > p");
 
-        difficultiesOptions.forEach((difficulty, i) => {
-            difficulty.addEventListener("click", () => {
-                this.difficulty = difficulties[i]; 
+    //     difficultiesOptions.forEach((difficulty, i) => {
+    //         difficulty.addEventListener("click", () => {
+    //             this.difficulty = difficulties[i]; 
                 
-                this.setPreFight();
-            });
+    //             this.setPreFight();
+    //         });
 
-        })
-    }
-    /**
-     * Showcases
-     * 
-     * The players stats
-     * The players status effects
-     * 
-     * The players options
-     */
-    setPreFight(){
-        game.innerHTML = this.player.showStats() + `<button class="fight-option button">Fight</button> <button class="dungeon-option"> Dungeon Demo </button>`;
+    //     })
+    // }
+    // /**
+    //  * Showcases
+    //  * 
+    //  * The players stats
+    //  * The players status effects
+    //  * 
+    //  * The players options
+    //  */
+    // setPreFight(){
+    //     game.innerHTML = this.player.showStats() + `<button class="fight-option button">Fight</button> <button class="dungeon-option"> Dungeon Demo </button>`;
 
-        const fightBtn = document.querySelector(".fight-option");
-        const dungeonBtn = document.querySelector(".dungeon-option");
+    //     const fightBtn = document.querySelector(".fight-option");
+    //     const dungeonBtn = document.querySelector(".dungeon-option");
         
-        fightBtn.addEventListener("click", () => {
-            const battle = new Battle();
-            battle.init(this, game, this.player, this.difficulty);
-        })
+    //     fightBtn.addEventListener("click", () => {
+    //         const battle = new Battle();
+    //         battle.init(this, game, this.player, this.difficulty);
+    //     })
 
-        dungeonBtn.addEventListener("click", () => {
-            const dungeon = new Dungeon(this, game);
-            dungeon.init();
-        })
-    }
+    //     dungeonBtn.addEventListener("click", () => {
+    //         const dungeon = new Dungeon(this, game);
+    //         dungeon.init();
+    //     })
+    // }
 
-    setDungeon(){
+    // setDungeon(){
 
-    }
-}
+    // }
+// }
 
 
 
-const main = new Main()
-main.selectCharacter();
+// const main = new Main()
+// main.selectCharacter();
